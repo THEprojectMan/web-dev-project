162004d1555afb0dc3e183909073a2c63cf08c56
THEprojectMan <amantharathnayaka2005@gmail.com>
2023-05-24 21:59:45 +0530

    updated shit

--- scripts/apply.js
+++ scripts/apply.js
@@ -1,26 +1,22 @@
 function storeJobRef1() {
-    var job02 = document.getElementById("job02").value;
-    localStorage.setItem("jobreference", job02);
-    location.href = "apply.html";
-  }
-  
-  function storeJobRef2() {
-    var job01 = document.getElementById("job01").value;
-    localStorage.setItem("jobreference", job01);
-    location.href = "apply.html";
-  }
-  
-  function storeApply() {
-    
-  }
-  
-  function init() {
-    var ref1 = document.getElementById("job02");
-    var ref2 = document.getElementById("job01");
-  
-    ref1.addEventListener("click", storeJobRef1);
-    ref2.addEventListener("click", storeJobRef2);
-  }
-  
-  window.onload = init;
-  
+  var job01 = document.getElementById("job01").value;
+  localStorage.setItem("jobreference", job01);
+  location.href = "apply.html";
+}
+
+function storeJobRef2() {
+  var job02 = document.getElementById("job02").value;
+  localStorage.setItem("jobreference", job02);
+  location.href = "apply.html";
+}
+
+
+function init() {
+  var ref1 = document.getElementById("job01");
+  var ref2 = document.getElementById("job02");
+
+  ref1.addEventListener("click", storeJobRef1);
+  ref2.addEventListener("click", storeJobRef2);
+}
+
+window.onload = init;
