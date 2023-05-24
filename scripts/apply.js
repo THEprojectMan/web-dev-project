cad0939c441807a210844bab4759ebf99a0072c3
Vikum Dabare <vikumdabare@gmail.com>
2023-05-24 22:18:39 +0530

    remove reference js file

--- scripts/apply.js
+++ scripts/apply.js
@@ -2,3 +2,11 @@ const navigatePage = (ref) => {
   localStorage.setItem("job_ref", ref);
   window.location.href = "apply.html";
 };
+
+const loadRef = () => {
+  refInput = document.getElementById("jobreference");
+  refValue = localStorage.getItem("job_ref");
+  if (refValue) refInput.value = refValue;
+};
+
+window.onload = () => loadRef();
--- scripts/reference.js
+++
@@ -1,7 +0,0 @@
-const loadRef = () => {
-  refInput = document.getElementById("jobreference");
-  refValue = localStorage.getItem("job_ref");
-  if (refValue) refInput.value = refValue;
-};
-
-window.onload = () => loadRef();
